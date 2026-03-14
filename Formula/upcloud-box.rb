6ec5618e112cc34fa44cad2adf504345370ead00
Ilja Kotirinta <ilja@mailbox.org>
2026-03-14 14:37:07 +0200

    chore: initialize upcloud-box Homebrew tap

---
+++ Formula/upcloud-box.rb
@@ -0,0 +1,20 @@
+class UpcloudBox < Formula
+  desc "Provision and deploy a secure Docker host on UpCloud"
+  homepage "https://github.com/suruaku/upcloud-box"
+  url "https://github.com/suruaku/upcloud-box/releases/download/v1.0.0/upcloud-box_v1.0.0_darwin_arm64.tar.gz"
+  sha256 "dec660714221fe1943571726d2278ab0ff2036cf261e04a499546762a9084362"
+  license "MIT"
+  version "1.0.0"
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  def install
+    bin.install "upcloud-box"
+  end
+
+  test do
+    output = shell_output("#{bin}/upcloud-box --version")
+    assert_match "v1.0.0", output
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Homebrew tap for `upcloud-box`.
+
+## Install
+
+```bash
+brew tap suruaku/tap
+brew install upcloud-box
+```
+
+## Supported platform
+
+- macOS arm64
