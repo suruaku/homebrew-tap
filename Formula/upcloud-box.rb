c0d59a4e071301f20e894aa7f4aac66790424ce3
Ilja Kotirinta <ilja@mailbox.org>
2026-03-14 14:44:33 +0200

    fix: satisfy Homebrew formula field ordering

--- Formula/upcloud-box.rb
+++ Formula/upcloud-box.rb
@@ -2,9 +2,9 @@ class UpcloudBox < Formula
   desc "Provision and deploy a secure Docker host on UpCloud"
   homepage "https://github.com/suruaku/upcloud-box"
   url "https://github.com/suruaku/upcloud-box/releases/download/v1.0.0/upcloud-box_v1.0.0_darwin_arm64.tar.gz"
+  version "1.0.0"
   sha256 "dec660714221fe1943571726d2278ab0ff2036cf261e04a499546762a9084362"
   license "MIT"
-  version "1.0.0"
 
   depends_on arch: :arm64
   depends_on :macos
