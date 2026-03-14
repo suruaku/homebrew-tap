d2702b4a975d386c400d8e2ab6cea8539e8b5856
Ilja Kotirinta <ilja@mailbox.org>
2026-03-14 15:18:07 +0200

    fix: bump formula to v1.0.2 with correct checksum

--- Formula/upcloud-box.rb
+++ Formula/upcloud-box.rb
@@ -3,7 +3,7 @@ class UpcloudBox < Formula
   homepage "https://github.com/suruaku/upcloud-box"
   url "https://github.com/suruaku/upcloud-box/releases/download/v1.0.2/upcloud-box_v1.0.2_darwin_arm64.tar.gz"
   version "1.0.2"
-  sha256 "52db45a174c65e96313156e4bd3273e5e2ca18d43f35e11e5f6d2598aa3d5698"
+  sha256 "055c54a6f9a7450de3e70ecbe8501758b968a3f135e8d53ad88dc3f717855a21"
   license "MIT"
 
   depends_on arch: :arm64
